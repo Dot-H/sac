b0b9e4da808d9612ba6b770ee15448c70689676d
Alexandre Bernard <alexandre.bernard@lse.epita.fr>
2018-03-15 23:49:03 +0100

    symbols: getting address of symbols dynamically

--- src/python-module/sac.py
+++ src/python-module/sac.py
@@ -10,11 +10,33 @@ sac = None
 
 def dump_objfile(objfiles):
     for f in objfiles:
-        if f.filename:
-            gdb.write(f.filename + '\n')
-            gdb.write(f.build_id + '\n')
-        else:
-            gdb.write("no filename\n", gdb.stderr)
+        try:
+            if f.filename:
+                gdb.write(f.filename + '\n')
+                gdb.write(f.build_id + '\n')
+                frame = gdb.selected_frame()
+                if not frame.is_valid():
+                    continue
+                sym_n_line = frame.find_sal()
+                sym = sym_n_line.symtab
+                if not sym or not sym.is_valid():
+                    continue
+
+                gdb.write("filename: " + sym.filename + '\n')
+            else:
+                gdb.write("no filename\n", gdb.stderr)
+        except gdb.error:
+            continue
+
+def read_func(funcname):
+        sym = gdb.lookup_symbol(funcname)[0]
+        if (not sym.is_valid() or not sym.is_function):
+            gdb.write("Symbol is not a valid function\n", gdb.STDERR)
+
+        addr = sym.value()
+        pprint(int(addr.address))
+
+        gdb.write(str(sym.print_name))
 
 class Listener (threading.Thread):
     def __init__(self, threadID, name, counter):
@@ -40,6 +62,7 @@ class Listener (threading.Thread):
                 try:
                     (clientsocket, address) = self.s.accept()
                     dump_objfile(gdb.objfiles())
+                    read_func("foo")
                     clientsocket.close()
                 except socket.error:
                     continue
@@ -49,7 +72,7 @@ class SacCommand (gdb.Command):
     "Command to update the code in real time."
     
     def __init__(self):
-        super (SacCommand, self).__init__("sac", gdb.COMMAND_USER,
+        super (SacCommand, self).__init__("sac", gdb.COMMAND_RUNNING,
                                           gdb.COMPLETE_NONE)
         self.listener = Listener(1, "FileListener", 1)
 
