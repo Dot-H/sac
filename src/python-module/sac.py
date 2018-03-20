7bc2ef0dd553721e8f7068aaef9dec40d6bfd8c2
Alexandre Bernard <alexandre.bernard@lse.epita.fr>
2018-03-20 18:58:56 +0100

    injection: add injection procedure

--- src/python-module/sac.py
+++ src/python-module/sac.py
@@ -4,35 +4,68 @@ import gdb
 import socket
 import threading
 from pprint import pprint
+import copy
 import sys
-
+ 
 sac = None
-"""open_shared_code = "\x48\x29\xf4\xcc\x49\x89\xf9\x48\x89\xe7\x48\x31\xf6\x48\xff\xc6\x41\xff\xd1\xcc"
+open_shared_code = b'\x48\x29\xf4\xcc\x49\x89\xf9\x48\x89\xe7\x48\x31\xf6\x48\xff\xc6\x41\xff\xd1\xcc'
 
-def open_shared(dlopen_addr, inject_addr, inferior, frame):
+def open_shared(dlopen_addr, inject_addr, inferior, frame, lib_path):
+    res = True
+
+    sv_regs = x86GenRegisters(frame)
+    inject_addr = sv_regs.rip #FIXME
     sv_buf = inferior.read_memory(inject_addr, len(open_shared_code))
-    sv_regs = gdb.parse_and_eval('info registers')
-    
-    new_regs = gdb.registers(sv_regs)
+    inferior.write_memory(inject_addr, open_shared_code)
+    pprint(sv_buf.hex())
+
+    new_regs = copy.copy(sv_regs)
     new_regs.rip = inject_addr
     new_regs.rdi = dlopen_addr
-    new_regs.rsi = len(open_shared_code)
-    gdb.write_regs(new_regs)
+    new_regs.rsi = len(lib_path) + 1
+    write_regs(new_regs, ["rip", "rdi", "rsi"])
 
-    gdb.cont()
+    gdb.execute("continue")
 
-    new_regs = gdb.read_regs()
-    inferior.write_memory(new_regs.rsp, open_shared_code)
+    new_regs = x86GenRegisters(frame)
+    inferior.write_memory(new_regs.rsp, lib_path + '\0')
 
-    gdb.cont()
+    gdb.execute("continue")
 
-    new_regs = gdb.read_regs()
+    new_regs = x86GenRegisters(frame)
     if new_regs.rax == 0:
-        return None
+        gdb.write("failed to load library\n", gdb.STDERR)
+        res = False
 
-    gdb.write_regs(sv_regs)
-    gdb.write_memory(sv_buf)
-    """
+    #FIXME generic way
+    write_regs(sv_regs, ["rax", 
+            "rax", 
+            "rbx", 
+            "rcx", 
+            "rdx", 
+            "rsi", 
+            "rdi", 
+            "rbp", 
+            "rsp", 
+            "r8", 
+            "r9", 
+            "r10", 
+            "r11", 
+            "r12", 
+            "r13", 
+            "r14", 
+            "r15", 
+            "rip", 
+            "eflags"])
+    inferior.write_memory(inject_addr, sv_buf)
+    gdb.execute("continue")
+    return res
+
+def write_regs(regs, to_write):
+    for reg in to_write:
+        cmd = "set ${0} = {1}".format(reg, hex(getattr(regs, reg)))
+        pprint(cmd)
+        gdb.execute(cmd)
 
 def dump_objfile(objfiles):
     for f in objfiles:
@@ -54,21 +87,23 @@ def dump_objfile(objfiles):
         except gdb.error:
             continue
 
-def read_func(funcname):
+def func_addr(funcname):
+    sym = gdb.lookup_global_symbol(funcname, gdb.SYMBOL_FUNCTIONS_DOMAIN)
+    if (not sym or not sym.is_valid() or not sym.is_function):
         try:
-            gdb.execute('info registers', True, True)
-            #pprint(sv_regs)
+            res = gdb.execute("p " + funcname, False, True)
+            addr_tab = res.split(' ')
+            # TODO Check for function name matching
+            addr = int(addr_tab[-2], 16)
+            pprint(hex(addr))
+            return addr
         except gdb.error:
-            gdb.write("Failed to parse and eval\n", gdb.STDERR)
-        sym = gdb.lookup_symbol(funcname)[0]
-        if (not sym or not sym.is_valid() or not sym.is_function):
             gdb.write("Symbol is not a valid function\n", gdb.STDERR)
-            return
+            return None
 
-        addr = sym.value()
-        pprint(int(addr.address))
-
-        gdb.write(str(sym.print_name))
+    addr = sym.value()
+    pprint(hex(int(addr.address)))
+    return int(addr.address)
 
 class x86GenRegisters:
     def __init__(self, frame=None):
@@ -93,67 +128,36 @@ class x86GenRegisters:
             self.rip    = 0
             self.eflags = 0
         else:
-            self.rax    = frame.read_register('rax')
-            self.rax    = frame.read_register('rax')
-            self.rbx    = frame.read_register('rbx')
-            self.rcx    = frame.read_register('rcx')
-            self.rdx    = frame.read_register('rdx')
-            self.rsi    = frame.read_register('rsi')
-            self.rdi    = frame.read_register('rdi')
-            self.rbp    = frame.read_register('rbp')
-            self.rsp    = frame.read_register('rsp')
-            self.r8     = frame.read_register('r8')
-            self.r9     = frame.read_register('r9')
-            self.r10    = frame.read_register('r10')
-            self.r11    = frame.read_register('r11')
-            self.r12    = frame.read_register('r12')
-            self.r13    = frame.read_register('r13')
-            self.r14    = frame.read_register('r14')
-            self.r15    = frame.read_register('r15')
-            self.rip    = frame.read_register('rip')
-            self.eflags = frame.read_register('eflags')
-
-class Listener (threading.Thread):
-    def __init__(self, threadID, name, counter):
-        threading.Thread.__init__(self)
-        self.threadID = threadID
-        self.name = name
-        self.counter = counter
-        self.s = None
-        self.stoprequest = threading.Event()
-
-    def join(self, timeout=None):
-        self.stoprequest.set()
-        if self.s != None:
-            self.s.close()
-        super(Listener, self).join(timeout)
-
-    def run(self):
-            self.s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-            self.s.setblocking(False)
-            self.s.bind((socket.gethostname(), 1111))
-            self.s.listen(5)
-            while not self.stoprequest.isSet():
-                try:
-                    (clientsocket, address) = self.s.accept()
-                    dump_objfile(gdb.objfiles())
-                    read_func("foo")
-                    clientsocket.close()
-                except socket.error:
-                    continue
-
+            self.rax    = int(frame.read_register('rax'))
+            self.rax    = int(frame.read_register('rax'))
+            self.rbx    = int(frame.read_register('rbx'))
+            self.rcx    = int(frame.read_register('rcx'))
+            self.rdx    = int(frame.read_register('rdx'))
+            self.rsi    = int(frame.read_register('rsi'))
+            self.rdi    = int(frame.read_register('rdi'))
+            self.rbp    = int(frame.read_register('rbp'))
+            self.rsp    = int(frame.read_register('rsp'))
+            self.r8     = int(frame.read_register('r8'))
+            self.r9     = int(frame.read_register('r9'))
+            self.r10    = int(frame.read_register('r10'))
+            self.r11    = int(frame.read_register('r11'))
+            self.r12    = int(frame.read_register('r12'))
+            self.r13    = int(frame.read_register('r13'))
+            self.r14    = int(frame.read_register('r14'))
+            self.r15    = int(frame.read_register('r15'))
+            self.rip    = int(frame.read_register('rip'))
+            self.eflags = int(frame.read_register('eflags'))
 
 class SacCommand (gdb.Command):
     "Command to update the code in real time."
-    
+
     def __init__(self):
         super (SacCommand, self).__init__("sac", gdb.COMMAND_RUNNING,
-                                          gdb.COMPLETE_NONE)
-        self.listener = Listener(1, "FileListener", 1)
+                                          gdb.COMPLETE_FILENAME)
 
     def invoke(self, arg, from_tty):
-        self.listener.start() 
-        try:
-            x86GenRegisters(gdb.selected_frame())
+        dlopen_addr = func_addr("__libc_dlopen_mode")
+        open_shared(dlopen_addr, 0, gdb.selected_inferior(),
+                gdb.selected_frame(), "/home/doth/EPITA/lse/sac/build/test.so")
 
-sac = SacCommand()
+SacCommand()
