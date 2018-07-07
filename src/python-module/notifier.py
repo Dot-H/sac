f86577eaad375fe766b1151e329a21b7879f1cbf
Alexandre Bernard <alexandre.bernard@lse.epita.fr>
2018-07-07 15:16:04 +0200

    notifier: add Notifier class and sigrt injection
    This combo is based on inotifier in order to connect the source code to sac.
    To do so, we create a deamon (Notifier) which will poll the inotify events
    from the source code. In case of modifications, the Notifier will inject
    a SIGRTMIN signal to the tracee. The signal will trigger an handler into sac
    and give it back the controle of the debugging session.
    
    Notifier and sac shared a modifications set which is populated by Notifier
    and then cleared by sac once it has built and injected the modifications.
    
    Signed-off-by: Alexandre Bernard <alexandre.bernard@lse.epita.fr>

---
+++ src/python-module/notifier.py
@@ -0,0 +1,74 @@
+import gdb
+import inotify.adapters
+import inotify.constants
+import os
+import signal
+import threading
+
+'''
+When modifying into vim, vim raises a combo:
+IN_MOVE_SELF, IN_ATTRIB, IN_DELETE_SELF, IN_IGNORED
+making inotify stop watching the files. We must therefore restart
+inotify and re-add all the files.
+'''
+
+class Notifier(threading.Thread):
+    def __init__(self, modifications):
+        threading.Thread.__init__(self)
+        self.cond = threading.Condition()
+        self.stoprequest = threading.Event()
+
+        self.i = inotify.adapters.Inotify()
+        self.i_rst = False
+        self.watches = set() # Has to store it here because of the vim bug
+
+        self.modifications = modifications # Must be a set of string
+        self.sigrt = signal.SIGRTMIN
+        self.sigrt_str = 'SIG{}'.format(self.sigrt)
+
+        # Stop when the main thread stops
+        self.setDaemon(True)
+
+
+    def run(self):
+        while True:
+            for event in self.i.event_gen():
+                self.cond.acquire()
+
+                if not event:
+                    if len(self.modifications) > 0:
+                        inf = gdb.selected_inferior()
+                        os.kill(inf.pid, self.sigrt)
+                        self.cond.wait()
+
+                    if self.i_rst:
+                        print("restart")
+                        self.restart_inotify()
+                        self.i_rst = False
+                        break
+
+                elif event:
+                    (_, type_names, path, filename) = event
+                    gdb.write("PATH=[{}] FILENAME=[{}] EVENT_TYPES={}\n".format(
+                              path, filename, type_names), gdb.STDERR)
+                    if type_names == ['IN_MODIFY'] or \
+                       type_names == ['IN_MOVE_SELF']:
+                        self.modifications.add(path)
+                    elif type_names == ['IN_IGNORED'] or \
+                         type_names == ['IN_MOVE_SELF']:
+                        self.i_rst = True
+
+                self.cond.release()
+
+
+
+    def add_watch(self, path, mask=inotify.constants.IN_ALL_EVENTS):
+        self.watches.add(path)
+        self.i.add_watch(path, mask)
+
+
+    def restart_inotify(self):
+        del(self.i)
+        self.i = inotify.adapters.Inotify()
+        for f in self.watches:
+            self.i.add_watch(f)
--- src/python-module/sac.py
+++ src/python-module/sac.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python
 
 import gdb
+import inotify.constants
 import os
 import sys
 
@@ -14,6 +15,7 @@ from families import *
 from gdbUtils import *
 from libInjection import *
 from linkMap import *
+from notifier import Notifier
 from patchSymbols import *
 from pprint import pprint
 
@@ -27,38 +29,87 @@ class SacCommand (gdb.Command):
         self.patches = {} # Dictionnary of couple (address, Patch)
         self.builds = {} # Dictionnary for building commands (filename, command)
         self.families = Families() # Dictionnary of couple (symbol, Browser)
+        self.modifications = set() # Contains source files to build
         self.default_build = ["gcc", "-c"]
+        self.notifier = Notifier(self.modifications)
+        self.notifier.start()
 
         gdb.execute('handle SIGSEGV nopass')
+
+        gdb.execute('handle %s stop nopass' % self.notifier.sigrt_str)
+
 
     def invoke(self, arg, from_tty):
         argv = gdb.string_to_argv(arg)
         if not len(argv): # Called by a hook
             return patch_symbol(self.patches)
 
-
         if argv[0] == "--build-file":
             return parseSac(argv[1], self.builds)
 
+        if argv[0] == "--add-files":
+            if len(argv[1:]) == 0:
+                gdb.write("No file given\n", gdb.STDERR)
+                return False
+
+            return self.add_files(argv[1:])
+
+        self.edit(argv)
+
+
+    def add_files(self, files):
+            inf = gdb.selected_inferior()
+            if inf.pid != 0 and not self.edit(files):
+                gdb.write("Could not add the files %s\n" % files, gdb.STDERR)
+                return False
+
+            for f in files:
+                self.notifier.add_watch(f)
+
+            return True
+        
+
+    def edit(self, paths):
         gdb.write("Building... ");
-        path = build(argv, self.builds, self.default_build)
+        path = build(paths, self.builds, self.default_build)
         if not path:
             gdb.write("Build failed\n", gdb.STDERR)
-            return None
+            return False
         gdb.write("Done\n");
 
         if not patch_objfile(path, self.patches, self.families):
             gdb.write("Failed to change {0}\n".format(path), gdb.STDERR)
 
+        return True
 
 
     def sigsegv_handler(self):
         cli_out = gdb.execute('p /x $_siginfo._sifields._sigfault.si_addr',
                               False, True)
         segv_addr = int(cli_out.split(' ')[-1], 16)
-        gdb.write("Segv at %s\n" % hex(segv_addr), gdb.STDERR)
+        self.families.try_patch(segv_addr)
 #        if not self.families.try_patch(segv_addr):
-#            gdb.execute('signal SIGSEGV')
+#            gdb.execute('signal SIGSEGV') #FIXME: Make gdb crash WTF
+        gdb.execute('continue')
+
+
+    def sigrt_handler(self):
+        inf = gdb.selected_inferior()
+        self.notifier.cond.acquire()
+
+        if inf.pid != 0:
+            res = self.edit(self.modifications)
+            if res:
+                # Empty the modifications
+                while len(self.modifications) > 0:
+                    f = self.modifications.pop()
+#                    self.notifier.i.remove_watch(f)
+#                    self.notifier.i.add_watch(f)
+
+        self.notifier.cond.notify()
+        self.notifier.cond.release()
+
+        return res
 
 
     def test(self):
@@ -104,8 +155,6 @@ def static_var(varname, value):
         return func
     return decorate
 
-
-
 if __name__ == '__main__':
     sac = SacCommand()
 
@@ -114,5 +163,8 @@ if __name__ == '__main__':
         if isinstance(event, gdb.SignalEvent):
             if event.stop_signal == 'SIGSEGV':
                 stop_handler.sac.sigsegv_handler()
+            elif event.stop_signal == stop_handler.sac.notifier.sigrt_str:
+                stop_handler.sac.sigrt_handler()
+
 
     gdb.events.stop.connect(stop_handler)
